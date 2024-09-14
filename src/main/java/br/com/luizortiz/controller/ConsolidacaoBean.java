7f06a8ff1042667b6755eb3fcdf39a05a246ebb4
LuizOrtiz96 <luizortiz2010@hotmail.com>
2024-09-14 10:49:04 -0300

    Alteração no persistence.xml

--- src/main/java/br/com/luizortiz/controller/ConsolidacaoBean.java
+++ src/main/java/br/com/luizortiz/controller/ConsolidacaoBean.java
@@ -1,13 +1,11 @@
 package br.com.luizortiz.controller;
 
-import br.com.luizortiz.model.Pessoa;
 import br.com.luizortiz.model.PessoaSalarioConsolidado;
 import br.com.luizortiz.service.ConsolidacaoService;
+import lombok.Getter;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.List;
@@ -19,18 +17,11 @@ public class ConsolidacaoBean implements Serializable {
     @EJB
     private ConsolidacaoService consolidacaoService;
 
+    @Getter
     private List<PessoaSalarioConsolidado> resultados;
-
-    @PostConstruct
-    public void init() {
-        consolidarSalarios();
-    }
 
     public void consolidarSalarios() {
         resultados = consolidacaoService.consolidarSalarios();
     }
 
-    public List<PessoaSalarioConsolidado> getResultados() {
-        return resultados;
-    }
 }
